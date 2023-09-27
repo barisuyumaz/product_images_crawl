4802b9389184fbfa85bd96b0ad6c8a5dc952f51a
barisuyumaz <baris.uyumaz@hotmail.com>
2023-09-27 11:36:28 +0300

    functionsBaris.py has updated

--- functionsBaris.py
+++ functionsBaris.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 import time
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'}
-
 #FUNCTIONS
 #MAIN FUNC---
 def request_n_soup_special_for_epey(link):
